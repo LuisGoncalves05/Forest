97484e2b7f5754e05ac0c4c8b1be5e266ee7ac5c
Luís Gonçalves <lumicosgon.lg@gmail.com>
2025-09-14 00:02:30 +0100

    Initial testing implementation integration with cmake build system

---
+++ tests/rbtTests.cpp
@@ -0,0 +1,40 @@
+#include <gtest/gtest.h>
+#include <cmath>
+#include "../trees.hpp"
+
+TEST(RBTRandomInsertTest, BasicAssertions) {
+    srand(time(NULL));
+    const int N = 1 << 21;
+
+    std::set<int> stlSet;
+    for (int i = 0; i < N; i++) {
+        stlSet.insert(rand() % (N << 3));
+    }
+
+    auto mySet = RBT<int>(stlSet.begin(), stlSet.end());
+    EXPECT_LE(mySet.maxHeight(), std::floor(2*std::log2(stlSet.size()+1)));
+
+    for (size_t i = 0; i < stlSet.size(); i++) {
+        int elements[2] = {*stlSet.begin(), *stlSet.rbegin()};
+        int stlMin = elements[0];
+        int stlMax = elements[1];
+        EXPECT_EQ(stlMin, mySet.minimum());
+        EXPECT_EQ(stlMax, mySet.maximum());
+                
+        EXPECT_EQ(mySet.find(stlMin), stlSet.count(stlMin));
+        EXPECT_EQ(mySet.find(stlMin), true);
+        EXPECT_EQ(mySet.find(stlMax), stlSet.count(stlMax));
+        EXPECT_EQ(mySet.find(stlMax), true);
+
+        int element = elements[i % 2];
+
+        EXPECT_EQ(mySet.find(element), stlSet.count(element));
+        EXPECT_EQ(mySet.find(element), true);
+
+        mySet.remove(element);
+        stlSet.erase(element);
+
+        EXPECT_EQ(mySet.find(element), stlSet.count(element));
+        EXPECT_EQ(mySet.find(element), false);
+    }
+}
